734ce9307553b79db2198178bd4cc1658b9a6b7b
Jayro Barbosa <jayrojr@gmail.com>
2020-01-30 09:31:07 -0300

    feature: React Navigation new files

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    SignUp,
+  })
+);
